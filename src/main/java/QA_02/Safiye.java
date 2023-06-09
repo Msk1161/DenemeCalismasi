3c61fea4a7422bb016a0f1d93e322521ba76ebf7
safiyeCK <safiyekara46@gmail.com>
2023-06-09 16:29:39 +0200

    d03

--- src/main/java/QA_02/Safiye.java
+++ src/main/java/QA_02/Safiye.java
@@ -4,5 +4,6 @@ public class Safiye {
     public static void main(String[] args) {
         System.out.println("umutsuz vakayim");
         System.out.println("ikinci deneme");
+        System.out.println("sanirim basardim :)");
     }
 }
