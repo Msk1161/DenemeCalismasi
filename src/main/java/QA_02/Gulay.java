65443cd275fd10a97136a2de24c0559bfa518528
Mustafa S <106470171+Msk1161@users.noreply.github.com>
2023-06-12 09:28:08 +0100

    Update Gulay.java

--- src/main/java/QA_02/Gulay.java
+++ src/main/java/QA_02/Gulay.java
@@ -1,6 +1,11 @@
 package QA_02;
 
 public class Gulay {
+  public static void main(String[] args) {
+
+        System.out.println("deneme satir");
+        System.out.println("bir deneme daha");
+    
   
   
   
@@ -9,16 +14,6 @@ public class Gulay {
   
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  }
+   
 }
