fc77a453282e2de3c94f1c4ae7e80a25787ba0ef
Mustafa S <106470171+Msk1161@users.noreply.github.com>
2023-06-12 09:24:05 +0100

    Update Mustafa.java

--- src/main/java/QA_02/Mustafa.java
+++ src/main/java/QA_02/Mustafa.java
@@ -4,8 +4,28 @@ public class Mustafa {
     public static void main(String[] args) {
         System.out.println("Ilk adim");
         System.out.println("Ikinci adim");
-        System.out.println("  ramazan ");
+        System.out.println("Ucuncu adim");
+        System.out.println("Dorduncu adim");
+        
+        
 
+// 10-15 mustafa
+
+        //dsdgg
+
+
+//17-22 gulay
+
+
+/*
+            git add .
+            git commit -m " "
+            git branch isim
+            git branch
+            git checkout isim
+            git merge master
+*/
+        
 
     }
 }
