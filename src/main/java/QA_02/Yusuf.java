63b457778754e7c7f8cc4b1c0482fbfbcfba0223
Mustafa S <106470171+Msk1161@users.noreply.github.com>
2023-06-12 09:21:47 +0100

    Update Yusuf.java

--- src/main/java/QA_02/Yusuf.java
+++ src/main/java/QA_02/Yusuf.java
@@ -1,6 +1,10 @@
 package QA_02;
 
 public class Yusuf {
+ public static void main(String[] args) {
+        System.out.println("Birinci adim");
+        System.out.println("Version 2");
+        System.out.println( "v3");
   
   
   
