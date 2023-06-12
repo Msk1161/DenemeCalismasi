c2229b188c961f4dbccf0bfda562b4dc2e8611af
Mustafa S <106470171+Msk1161@users.noreply.github.com>
2023-06-12 09:26:23 +0100

    Update and rename Sena to Sena.java

---
+++ src/main/java/QA_02/Sena.java
@@ -0,0 +1,17 @@
+package QA_02;
+
+public class Sena {
+    public static void main(String[] args) {
+        System.out.println("Sena");
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    }
+}
